d87dc117623f8fcec37dfe1e1269e5bb728d45ee
StijnCSS <stijnzwaard@gmail.com>
2025-04-17 08:48:41 +0200

    Jokes generate

--- client/index.js
+++ client/index.js
@@ -5,3 +5,15 @@ console.log('Hello, world!');
 const now = new Date();
 const time = now.toLocaleTimeString();
 document.getElementById('time').textContent = time;
+
+
+// const jokeText = {{ joke | json }};
+//   document.querySelectorAll('[data-rating]').forEach(button => {
+//     button.addEventListener('click', () => {
+//       const rating = button.dataset.rating;
+//       const stored = JSON.parse(localStorage.getItem('jokes') || '[]');
+//       stored.push({ joke: jokeText, rating });
+//       localStorage.setItem('jokes', JSON.stringify(stored));
+//       window.location.href = '/';
+//     });
+//   });
